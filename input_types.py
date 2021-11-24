6dc7ae1a5e92055dd8eab7c396450f3eec900a1f
OlegStrelciuc <93653731+OlegStrelciuc@users.noreply.github.com>
2021-11-24 10:04:37 +0200

    Add files via upload

---
+++ input_types.py
@@ -0,0 +1,17 @@
+def inputInt(message):
+    return int(input(message))
+
+
+def inputFloat(message):
+    return float(input(message))
+
+
+def inputBoolean(message):
+    return bool(input(message))
+
+
+n = inputInt("Enter the first integer: ")
+m = inputInt("Enter the second integer: ")
+
+
+print(n+m)
